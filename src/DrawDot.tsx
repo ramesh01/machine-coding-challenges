404d34a852942f0f1062d2bea72a9c4b9191d9ea
rameshhb37 <rameshhb37@gmail.com>
2023-10-03 21:51:09 +0530

    Misc: Added Dot interface

--- src/DrawDot.tsx
+++ src/DrawDot.tsx
@@ -1,30 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 /**
-    * Draw a dot on mouse click
-    */
+ * Draw a dot on mouse click
+ */
+interface Dot {
+  x: number;
+  y: number;
+}
 const DrawDot = () => {
-
-  const [dots, setDots] = useState<any>([]);
+  const [dots, setDots] = useState<Dot[]>([]);
 
   const getCoordinates = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const x = e.clientX;
     const y = e.clientY;
-    const newDot = { x, y };
+    const newDot: Dot = { x, y };
     setDots([...dots, newDot]);
-  }
+  };
   return (
-    <div className='draw-dot-container' onClick={(e) => getCoordinates(e)}>
+    <div className="draw-dot-container" onClick={(e) => getCoordinates(e)}>
       DrawDot
       {dots.map((dot: any, index: number) => (
         <div
           key={index}
-          className='dot'
+          className="dot"
           style={{ left: dot.x, top: dot.y }}
         ></div>
-      )
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default React.memo(DrawDot);
