7eeaf82c40d7d9e462333567ae04cd6ecb0a4b14
rameshhb37 <rameshhb37@gmail.com>
2024-02-28 00:25:16 +0530

    feat: countdown timer component in-progress

--- src/pages/CountDownTimer/CountDownTimer.tsx
+++ src/pages/CountDownTimer/CountDownTimer.tsx
@@ -1,7 +1,24 @@
 const CountDownTimer = () => {
-  return (
-    <div>CountDownTimer</div>
-  )
-}
+	return (
+    <>
+		<div className="countdown-container">
+			<div className="countdown-item">
+				<input placeholder="HH" maxLength={2} />
+			</div>
+			<div className="colon">:</div>
+			<div className="countdown-item">
+				<input placeholder="MM" maxLength={2} />
+			</div>
+			<div className="colon">:</div>
+			<div className="countdown-item">
+				<input placeholder="SS" maxLength={2} />
+			</div>
+		</div>
+    <div>
+      <button type="button">Start</button>
+    </div>
+    </>
+	);
+};
 
 export default CountDownTimer;
